512d0a1f1b125ef2785570748a7ae8f6dea45454
rishabh <firestrom668@gmail.com>
2024-05-23 21:17:04 +0530

    This is commit from an another guy

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This line is the new addition to the code");
+		System.out.println("Now will push this updated code to github");
+	}
+
+}
